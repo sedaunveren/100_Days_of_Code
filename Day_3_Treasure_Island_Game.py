279e3dfb0fb62162e5a603734986a9f0757022c7
= <=>
2022-09-27 18:39:06 +0300

    Treasure Island Game is created

---
+++ Day_3_Treasure_Island_Game.py
@@ -0,0 +1,48 @@
+## TREASURE ISLAND GAME ##
+
+from turtle import left
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+across = input("'You're at a crossroad. Where do you want to go? Type 'left' or 'right'")
+across.lower()
+if across == "left":
+    work = input("'You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.'")
+    work.lower()
+    if work == "wait":
+        door =input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?")
+        door.lower()
+        if door == "yellow":
+            print("You Win!")
+        elif door == "red":
+            print("Burned by fire.Game Over.")
+        elif door == "blue":
+            print("Eaten by beasts.Game Over.")
+        else:
+            print("Game Over.")
+    else:
+        print("Attacked by trout.Game Over.")
+else:
+    print("Fall into a hole.Game Over.")
+

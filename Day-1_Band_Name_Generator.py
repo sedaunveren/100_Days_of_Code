9c7f737605361605c75da9e1e7ae220a7e312175
= <=>
2022-09-27 18:35:59 +0300

    Band Name Generator is created

---
+++ Day-1_Band_Name_Generator.py
@@ -0,0 +1,10 @@
+#print("Hello World!\nHello world!\nHello world!")
+#print("Hello" + " " + "Seda")
+#print("Hello s" + input("What is your name"))
+
+print("Welcome to the Band Name Generator.")
+print("What's name of the city you grew up in?")
+a = input()
+print("What's your pet's name?")
+b = input()
+print("Your band name could be", a +" "+ b )
